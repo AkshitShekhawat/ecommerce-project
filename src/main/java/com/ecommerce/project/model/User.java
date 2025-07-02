0ed89fd391f23bad63d6aeace1d552d7f360ec4c
AkshitShekhawat <akw17439@gmail.com>
2025-07-02 20:10:14 +0530

    Implement Users
    1.Address
    2.Role
    3.user
    4.AppRole as enum

---
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -0,0 +1,40 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+//@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    private String userName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    public User(Long userId, String password, String email, String userName) {
+        this.password = password;
+        this.email = email;
+        this.userName = userName;
+    }
+}

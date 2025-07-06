5fa20080a1393d9f47209d0a3bd7b4a74af3fbb0
AkshitShekhawat <akw17439@gmail.com>
2025-07-06 18:32:18 +0530

    Adding Product to the Cart

--- src/main/java/com/ecommerce/project/repository/CartRepository.java
+++ src/main/java/com/ecommerce/project/repository/CartRepository.java
@@ -2,6 +2,12 @@ package com.ecommerce.project.repository;
 
 import com.ecommerce.project.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
+    Cart findCartByEmail(String email);
 }
+
+// c is the object that we are retrieving from the cart
+//?1 -->> it indicate we are (((String email))) passing as a first parameter

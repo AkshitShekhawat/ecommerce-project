ea5099390eff455f12594b3656fc8bf579925bb6
AkshitShekhawat <akw17439@gmail.com>
2025-07-25 11:34:47 +0530

    frontend-project and confi done

---
+++ ecom-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { FaBeer } from 'react-icons/fa'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 class="flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold">
+    Hello world! <FaBeer />
+  </h1>
+    </div>
+  )
+}
+
+export default App

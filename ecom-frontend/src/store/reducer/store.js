3729a836892877eb3fd455f651987e5898a24aee
AkshitShekhawat <akw17439@gmail.com>
2025-08-05 15:59:56 +0530

    Setting up Redux For User Authentication

--- ecom-frontend/src/store/reducer/store.js
+++ ecom-frontend/src/store/reducer/store.js
@@ -2,12 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./productReducer";
 import { errorReducer } from "./errorReducer";
 import { cartReducer } from "./cartReducer";
+import { authReducer } from "./authReducer";
 
+const user = localStorage.getItem("auth")
+    ?JSON.parse(localStorage.getItem("auth"))
+    : [];
+
+//Fetching cartItems form localStorage
 const cartItems = localStorage.getItem("cartItems")
     ?JSON.parse(localStorage.getItem("cartItems"))
     : [];
 
 const initialState = {
+    auth: { user: user},
     carts: { cart: cartItems },
 };
 
@@ -16,6 +23,7 @@ export const store = configureStore({
          products: productReducer,
          errors: errorReducer,
          carts: cartReducer,
+         auth: authReducer
     },
     preloadedState: initialState,
 });

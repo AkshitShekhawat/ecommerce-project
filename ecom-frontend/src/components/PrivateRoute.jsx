731b81b067df27b406d448d0eb66c14a51e501c0
AkshitShekhawat <akw17439@gmail.com>
2025-08-06 01:07:01 +0530

    Redirecting User to Home After Login

--- ecom-frontend/src/components/PrivateRoute.jsx
+++ ecom-frontend/src/components/PrivateRoute.jsx
@@ -6,7 +6,7 @@ const PrivateRoute = ({ publicPage = false }) => {
   const { user } = useSelector((state) => state.auth);
 
   if (publicPage) {
-    return user ? <Navigate to="/profile" /> : <Outlet />; // if user is loggedIn and try to access login page then it will be redirected to its profile page
+    return user ? <Navigate to="/" /> : <Outlet />; // if user is loggedIn and try to access login page then it will be redirected to its profile page
   }
 
   return user ? <Outlet /> : <Navigate to="/login" />;  // this is the logic for private pages it redirected the unauthenticated user to login page

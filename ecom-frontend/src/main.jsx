5c0ccc68957b278ff6d98545e50cbc6a3da2955e
AkshitShekhawat <akw17439@gmail.com>
2025-08-08 22:34:52 +0530

    Getting Rid of the Strict Mode

--- ecom-frontend/src/main.jsx
+++ ecom-frontend/src/main.jsx
@@ -8,8 +8,6 @@ import store from './store/reducer/store.js'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <StrictMode>
       <App />
-    </StrictMode>
   </Provider>,
 )

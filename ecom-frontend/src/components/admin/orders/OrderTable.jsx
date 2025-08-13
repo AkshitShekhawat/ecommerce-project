af7f2184f1196a446c7bc980b9700169c10463b1
AkshitShekhawat <akw17439@gmail.com>
2025-08-13 18:56:21 +0530

    Designing the Admin Orders List Layout

--- ecom-frontend/src/components/admin/orders/OrderTable.jsx
+++ ecom-frontend/src/components/admin/orders/OrderTable.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import { DataGrid } from '@mui/x-data-grid'
+import { adminProductTableColumn } from '../../helper/tableColumn';
+import { useState } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
 
-const OrderTable = () => {
+const OrderTable = ({ adminOrder, pagination }) => {
+  const [currentPage, setCurrentPage] = useState(
+    pagination?.pageNumber + 1 || 1
+  );
+
+  const [searchParams] = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+  const pathname = useLocation().pathname;
+
+const tableRecords = adminOrder?.map((item) => {
+  return {
+    id: item.orderId,
+    email: item.email,
+    totalAmount: item.totalAmount,
+    status: item.orderStatus,
+    date: item.orderDate,
+  }
+});
+
+const handlePaginationChange = (paginationModel) => {
+  const page = paginationModel.page + 1;
+  setCurrentPage(page);
+  params.set("page", page.toString());
+  navigate(`${pathname}?${params}`)
+}
+
   return (
-    <div>OrderTable</div>
+    <div>
+      <h1 className='text-slate-800 text-3xl text-center font-bold pb-6 uppercase'>
+        All Orders
+      </h1>
+
+      <div>
+        <DataGrid
+          rows={tableRecords}
+          columns={adminProductTableColumn}
+          paginationMode='server'
+          rowCount={pagination?.totalElements || 0} 
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: pagination?.pageSize || 10,
+                  page: currentPage - 1,
+              },
+            },
+          }}
+          onPaginationModelChange={handlePaginationChange}
+          disableRowSelectionOnClick
+          disableColumnResize
+          pageSizeOptions={[pagination?.pageSize || 10]}
+          pagination
+          paginationOptions={{
+              showFirstButton: true,
+              showLastButton: true,
+              hideNextButton: currentPage === pagination?.totalPages,
+            }}
+      />
+      </div>
+    </div>
   )
 }
 
---
+++ ecom-frontend/src/components/helper/tableColumn.jsx
@@ -0,0 +1,93 @@
+import { FaEdit } from "react-icons/fa";
+
+export const adminProductTableColumn = [
+  {
+    sortable: false,
+    disableColumnMenu: true,
+    field: "id",
+    headerName: "orderId",
+    minWidth: 180,
+    headerAlign: "center",
+    editable: false,
+    headerClassName: "text-black font-semibold border",
+    cellClassName: "text-slate-700 font-normal border",
+    renderheader: (params) => <span className='text-center'>Order ID</span>
+  },
+  {
+    //Column for customer email.
+    sortable: false,
+    disableColumnMenu: true,
+    field: "email",
+    headerName: "Email",
+    align: "center",
+    minWidth: 250,
+    headerAlign: "center",
+    editable: false,
+    headerClassName: "text-black font-semibold text-center border",
+    cellClassName: "text-slate-700 font-normal border text-center",
+    renderheader: (params) => <span className='text-center'>Email</span>
+  },
+  {
+    //Cloumn for showing total amount of the order.
+    disableColumnMenu: true,
+    field: "totalAmount",
+    headerName: "Total Amount",
+    headerAlign: "center",
+    align: "center",
+    width: 200,
+    editable: false,
+    sortable: true,
+    headerClassName: "text-black font-semibold text-center border",
+    cellClassName: "text-slate-700 font-normal border text-center",
+    renderheader: (params) => <span className='text-center'>Total Amount</span>
+  },
+  {
+    //Cloumn for showing total amount of the order.
+    disableColumnMenu: true,
+    field: "status",
+    headerName: "Status",
+    headerAlign: "center",
+    align: "center",
+    width: 200,
+    editable: false,
+    sortable: false,
+    headerClassName: "text-black font-semibold text-center border",
+    cellClassName: "text-slate-700 font-normal border text-center",
+    renderheader: (params) => <span className='text-center'>Status</span>
+  },
+  {
+    //Cloumn to display order status  (e.g, Pending, Shipped).
+    disableColumnMenu: true,
+    field: "date",
+    headerName: "Order Date",
+    headerAlign: "center",
+    align: "center",
+    width: 200,
+    editable: false,
+    sortable: false,
+    headerClassName: "text-black font-semibold text-center border",
+    cellClassName: "text-slate-700 font-normal border text-center",
+    renderheader: (params) => <span className='text-center'>Order Date</span>
+  },
+  {
+    field: "action",
+    headerName: "Action",
+    headerAlign: "center",  
+    editable: false,
+    headerClassName: "text-black font-semibold text-center",
+    cellClassName: "text-slate-700 font-normal",
+    sortable: false,
+    width: 250,
+    renderheader: (params) => <span>Action</span>,
+    renderCell:(params) => {
+      return (
+        <div className='flex justify-center items-center space-x-2 h-full pt-2'>
+          <button className='flex items-center bg-blue-500 text-white px-4 h-9 rounded-md mb-2'>
+            <FaEdit className='mr-2'/>
+            Edit
+          </button>
+        </div>
+      );
+    }
+  },
+];

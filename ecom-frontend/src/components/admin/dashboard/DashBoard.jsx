5d045b9498a8d88281648b46d64ed69a921f8eb7
AkshitShekhawat <akw17439@gmail.com>
2025-08-13 12:17:13 +0530

    Fetching Dynamic Analytics Data for the Dashboard

--- ecom-frontend/src/components/admin/dashboard/DashBoard.jsx
+++ ecom-frontend/src/components/admin/dashboard/DashBoard.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import DashboardOverview from './DashboardOverview'
 import { FaBoxOpen, FaDollarSign, FaShoppingCart } from 'react-icons/fa'
+import { useDispatch, useSelector } from 'react-redux';
+import { analyticsAction } from '../../../store/actions';
+import ErrorPage from '../../shared/ErrorPage';
+import Loader from '../../shared/Loader';
 
 const DashBoard = () => {
+  const dispatch = useDispatch();
+  const {isLoading, errorMessage} = useSelector((state) => state.errors);
 
-  const { productCount, totalRevenue, totalOrders } = { "productCount":  "13", "totalRevenue": "8680.98", "totalOrders": "4"};
+  const { 
+    analytics: { productCount, totalRevenue, totalOrders },
+   } = useSelector((state) => state.admin);
+
+   useEffect(() => {
+    dispatch(analyticsAction());
+   }, [dispatch]);
+
+   if (isLoading) {
+    return <Loader />
+   }
+
+   if (errorMessage) {
+    return <ErrorPage message={errorMessage}/>;
+   }
+
   return (
     <div>
       <div className='flex md:flex-row mt-8 flex-col lg:justify-between 

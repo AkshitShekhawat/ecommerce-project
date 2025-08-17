7575089d61c14e6b6f534b0ce5ec7f26d5eb8234
AkshitShekhawat <akw17439@gmail.com>
2025-08-18 00:05:46 +0530

    Backend API: Fetching Orders for the Logged-In Seller

--- sb-ecom-backend/src/main/java/com/ecommerce/project/service/OrderServiceImpl.java
+++ sb-ecom-backend/src/main/java/com/ecommerce/project/service/OrderServiceImpl.java
@@ -7,6 +7,7 @@ import com.ecommerce.project.payload.OrderDTO;
 import com.ecommerce.project.payload.OrderItemDTO;
 import com.ecommerce.project.payload.OrderResponse;
 import com.ecommerce.project.repository.*;
+import com.ecommerce.project.util.AuthUtil;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +47,9 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     ProductRepository productRepository;
+
+    @Autowired
+    AuthUtil authUtil;
 
     @Override
     @Transactional
@@ -146,5 +150,41 @@ public class OrderServiceImpl implements OrderService {
         order.setOrderStatus(status);
         orderRepository.save(order);
         return modelMapper.map(order, OrderDTO.class);
+    }
+
+    @Override
+    public OrderResponse getAllSellerOrders(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
+        Sort sortByAndOrder = sortOrder.equalsIgnoreCase("asc")
+                ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+        Pageable pageDetails = PageRequest.of(pageNumber, pageSize, sortByAndOrder);
+
+        User seller = authUtil.loggedInUser();
+
+        Page<Order> pageOrders = orderRepository.findAll(pageDetails);
+
+        List<Order> sellerOrders = pageOrders.getContent().stream()
+                .filter(order -> order.getOrderItems().stream()
+                        .anyMatch(orderItem -> {
+                            var product = orderItem.getProduct();
+                            if (product == null || product.getUser() == null) {  //So if the product is not associated with any seller then in that case don't take that product and just skip it.
+                                return false;
+                            }
+                            return product.getUser().getUserId().equals(
+                                    seller.getUserId());
+                        }))
+                .toList();
+
+        List<OrderDTO> orderDTOs = sellerOrders.stream()
+                .map(order -> modelMapper.map(order, OrderDTO.class))
+                .toList();
+        OrderResponse orderResponse = new OrderResponse();
+        orderResponse.setContent(orderDTOs);
+        orderResponse.setPageNumber(pageOrders.getNumber());
+        orderResponse.setPageSize(pageOrders.getSize());
+        orderResponse.setTotalElements(pageOrders.getTotalElements());
+        orderResponse.setTotalPages(pageOrders.getTotalPages());
+        orderResponse.setLastPage(pageOrders.isLast());
+        return orderResponse;
     }
 }
